2cbf20ffdca3e575ddb871ef0ad2f0523df0becb
kurtnettle <89929240+KurtNettle@users.noreply.github.com>
2023-11-13 21:27:43 +0600

    [frontend] improve file list table. + made file size human readable. + delete button is now functional. + duplicate file filter added. + minor css tweaks.

--- src/main/resources/public/js/utils.js
+++ src/main/resources/public/js/utils.js
@@ -1,23 +1,42 @@
-function gen_file_list_table (files) {
+function is_duplicate(file_name, file_size) {
+  flag = false;
+  $("div.upload-file-list table.file-list tbody tr").each(function () {
+    const name = $(this).find('.file-name').text();
+    const size = $(this).find("td").eq(1).attr('size-in-bytes')
+    console.log(`${file_name == name && size == file_size}`)
+    if (file_name == name && size == file_size) flag = true;
+  })
+  return flag;
+}
+
+function gen_file_list_table(files) {
   const table = document.querySelector('div.upload-file-list table.file-list')
   table.style.display = '';
   const tbody = table.querySelector("tbody");
+
+  const sizeFormatter = new Intl.NumberFormat([], {
+    style: 'unit',
+    unit: 'byte',
+    notation: "compact",
+    unitDisplay: "narrow",
+  })
+
   let i = 0
   for (const file of files) {
-    const html = `<td>${file.name}</td>
-    <td>${file.size}</td>
+    const html = `<td class="file-name">${file.name}</td>
+    <td class="file-size" size-in-bytes=${file.size}>${sizeFormatter.format(file.size)}</td>
     <td class="center-align">
-    <a onclick="remove_file();" class="waves-effect waves-red btn-flat">
+    <a onclick="$(this).closest('tr').remove();" class="waves-effect waves-red btn-flat">
     <i class="material-icons red-text">delete</i>
     </a>
     </td>`
-    const row = tbody.insertRow(i)
-    row.innerHTML = html
-    i += 1
+
+    if (!is_duplicate(file.name, file.size)) {
+      const row = tbody.insertRow(i)
+      row.innerHTML = html
+      i += 1
+    } else {
+      M.toast({ html: `<span><b>${file.name}</b> was already added.</span>` })
+    }
   }
-}
-
-
-function remove_file(e){
-  console.log(e);
 }
